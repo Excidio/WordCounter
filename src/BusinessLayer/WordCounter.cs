3879f9436c3978e459f627d4cc3265224ee87551
Excidio <popov095@gmail.com>
2017-06-18 22:38:21 +0500

    Word counter added

---
+++ src/BusinessLayer/WordCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordCounter.BusinessLayer
+{
+    public class WordCounter
+    {
+        private static readonly LineParser LineParser = new LineParser();
+        
+        public IDictionary<string, int> Calculate(string fileName)
+        {
+            var taskList = new LinkedList<Task>();
+            var dictionary = new FrequencyDictionary<string>();
+
+            foreach (var line in File.ReadLines(fileName))
+            {
+                taskList.AddLast(Task.Factory.StartNew(() => ProcessLine(dictionary, line)));
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+            return dictionary;
+        }
+
+        private static void ProcessLine(FrequencyDictionary<string> dictionary, string value)
+        {
+            foreach (var val in LineParser.Parse(value))
+            {
+                dictionary.Add(val.ToLower());
+            }
+        }
+    }
+}
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,37 +1,17 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using WordCounter.BusinessLayer;
 
 namespace WordCounter.ConsoleApp
 {
     public class Program
     {
-        private static readonly FrequencyDictionary<string> Dictionary = new FrequencyDictionary<string>();
-        private static readonly LineParser LineParser = new LineParser();
+        private static readonly BusinessLayer.WordCounter WordCounter = new BusinessLayer.WordCounter();
 
         public static void Main(string[] args)
         {
-            var taskList = new LinkedList<Task>();
+            var result = WordCounter.Calculate(@"D:\Test\1.txt");
 
-            foreach (var line in File.ReadLines(@"D:\Test\1.txt"))
-            {
-                taskList.AddLast(Task.Factory.StartNew(() => ProcessLine(line)));
-            }
-
-            Task.WaitAll(taskList.ToArray());
-
-            File.WriteAllLines(@"D:\Test\res.txt", Dictionary.OrderByDescending(p => p.Value).Select(p => $"{p.Key},{p.Value}\n"));
-        }
-
-        private static void ProcessLine(string value)
-        {
-            foreach (var val in LineParser.Parse(value))
-            {
-                Dictionary.Add(val.ToLower());
-            }
+            File.WriteAllLines(@"D:\Test\res.txt", result.OrderByDescending(p => p.Value).Select(p => $"{p.Key},{p.Value}\n"));
         }
     }
 }
