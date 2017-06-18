bb26bc502541f8ce0fe046297ae33359c7603062
Excidio <popov095@gmail.com>
2017-06-18 16:13:59 +0500

    parallel processing

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,28 +1,39 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using WordCounter.BusinessLayer;
 
 namespace WordCounter.ConsoleApp
 {
     public class Program
     {
+        private static readonly FrequencyDictionary<string> Dictionary = new FrequencyDictionary<string>();
+
         public static void Main(string[] args)
         {
-            var dictionary = new FrequencyDictionary<string>();
+            var taskList = new List<Task>();
 
             foreach (var line in File.ReadLines(@"D:\Test\1.txt"))
             {
-                var values = Regex
-                    .Matches(line, "\\w+")
-                    .Cast<Match>()
-                    .Select(m => m.Value)
-                    .ToArray();
+                taskList.Add(Task.Factory.StartNew(() => Process(line)));
+            }
 
-                foreach (var val in values)
-                {
-                    dictionary.Add(val.ToLower());
-                }
+            Task.WaitAll(taskList.ToArray());
+        }
+
+        private static void Process(string value)
+        {
+            var values = Regex
+                .Matches(value, "\\w+")
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
+
+            foreach (var val in values)
+            {
+                Dictionary.Add(val.ToLower());
             }
         }
     }
