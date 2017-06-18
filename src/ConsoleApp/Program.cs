e7dd7238f1bb5bd1f6cc1d8903a71fc50cfd8c59
Excidio <popov095@gmail.com>
2017-06-18 14:42:00 +0500

    file processing added

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace WordCounter.ConsoleApp
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WordCounter.BusinessLayer;
+
+namespace WordCounter.ConsoleApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var dictionary = new FrequencyDictionary<string>();
+
+            foreach (var line in File.ReadLines(@"D:\Test\1.txt"))
+            {
+                var values = Regex
+                    .Matches(line, "\\w+")
+                    .Cast<Match>()
+                    .Select(m => m.Value)
+                    .ToArray();
+
+                foreach (var val in values)
+                {
+                    dictionary.Add(val.ToLower());
+                }
+            }
         }
     }
 }
