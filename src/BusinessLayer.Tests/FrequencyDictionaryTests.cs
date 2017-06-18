68f8cf68f1b3ae39e05e3b00505a519fcb525bbb
Excidio <popov095@gmail.com>
2017-06-18 14:29:33 +0500

    FrequencyDictionary + tests

---
+++ src/BusinessLayer.Tests/FrequencyDictionaryTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace WordCounter.BusinessLayer.Tests
+{
+    [TestFixture]
+    public class FrequencyDictionaryTests
+    {
+        [TestCase(1, "aaa")]
+        [TestCase(1, "a", "aa", "aaa")]
+        [TestCase(3, "aaa", "aaa", "aaa")]
+        [TestCase(3, "aaa", "aba", "aaa", "aaa")]
+        [TestCase(5, "aaa", "aaa", "aaa", "aaa", "aaa")]
+        public void AddTest(int result, params string[] values)
+        {
+            var frequencyDictionary = new FrequencyDictionary<string>();
+
+            foreach (var value in values)
+            {
+                frequencyDictionary.Add(value);
+            }
+
+            Assert.AreEqual(result, frequencyDictionary.GetValue(values.First()));
+        }
+
+        [TestCase(0, "aaa")]
+        [TestCase(0, "a")]
+        [TestCase(0, "ccc")]
+        public void GetTest(int result, string value)
+        {
+            var frequencyDictionary = new FrequencyDictionary<string>();
+
+            Assert.AreEqual(result, frequencyDictionary.GetValue(value));
+        }
+    }
+}
---
+++ src/BusinessLayer/FrequencyDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace WordCounter.BusinessLayer
+{
+    public class FrequencyDictionary<T>
+    {
+        private readonly ConcurrentDictionary<T, int> _dictionary = new ConcurrentDictionary<T, int>();
+
+        public void Add(T key)
+        {
+            _dictionary.AddOrUpdate(key, 1, (v, i) => ++i);
+        }
+
+        public int GetValue(T key)
+        {
+            _dictionary.TryGetValue(key, out int result);
+
+            return result;
+        }
+    }
+}
